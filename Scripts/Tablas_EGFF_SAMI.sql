f9232b33bd190a2ed439dc7fda5f89a252014b52
Kenet Orellana <86451633+KenetOrellana@users.noreply.github.com>
2024-01-15 21:27:00 -0600

    Tablas_EGFF_SAMI
    Tablas_EGFF_SAMI

---
+++ Scripts/Tablas_EGFF_SAMI.sql
@@ -0,0 +1,164 @@
+/*PARA TABLAS SAMI_13*/
+=[SAMI_EGFF_13].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_13].[Nombre del Municipio])
+=[SAMI_EGFF_13].[Fuente]
+=[SAMI_EGFF_13].[Grupo Gasto]
+=[SAMI_EGFF_13].[Nombre]
+=Mayúsculas([SAMI_EGFF_13].[Fuente] + " - " + [SAMI_EGFF_13].[Descripcion Fuente])
+=[SAMI_EGFF_13].[Asignado]
+=[Modificación_13]
+=[SAMI_EGFF_13].[Vigente]
+=[SAMI_EGFF_13].[Devengado]
+=[SAMI_EGFF_13].[Pagado]
+=Si(EsError([%_de_Ejecución_13]);"División 0/0";[%_de_Ejecución_13]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_13];1;[Tamaño_Barras_13]));"División 0/0";Subcadena([Relleno_Gráfico_13];1;[Tamaño_Barras_13]))
+
+/*PARA TABLAS SAMI_14*/
+=[SAMI_EGFF_14].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_14].[Nombre del Municipio])
+=[SAMI_EGFF_14].[Fuente]
+=[SAMI_EGFF_14].[Grupo Gasto]
+=[SAMI_EGFF_14].[Nombre]
+=Mayúsculas([SAMI_EGFF_14].[Fuente] + " - " + [SAMI_EGFF_14].[Descripcion Fuente])
+=[SAMI_EGFF_14].[Asignado]
+=[Modificación_14]
+=[SAMI_EGFF_14].[Vigente]
+=[SAMI_EGFF_14].[Devengado]
+=[SAMI_EGFF_14].[Pagado]
+=Si(EsError([%_de_Ejecución_14]);"División 0/0";[%_de_Ejecución_14]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_14];1;[Tamaño_Barras_14]));"División 0/0";Subcadena([Relleno_Gráfico_14];1;[Tamaño_Barras_14]))
+
+/*PARA TABLAS SAMI_15*/
+=[SAMI_EGFF_15].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_15].[Nombre del Municipio])
+=[SAMI_EGFF_15].[Fuente]
+=[SAMI_EGFF_15].[Grupo Gasto]
+=[SAMI_EGFF_15].[Nombre]
+=Mayúsculas([SAMI_EGFF_15].[Fuente] + " - " + [SAMI_EGFF_15].[Descripcion Fuente])
+=[SAMI_EGFF_15].[Asignado]
+=[Modificación_15]
+=[SAMI_EGFF_15].[Vigente]
+=[SAMI_EGFF_15].[Devengado]
+=[SAMI_EGFF_15].[Pagado]
+=Si(EsError([%_de_Ejecución_15]);"División 0/0";[%_de_Ejecución_15]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_15];1;[Tamaño_Barras_15]));"División 0/0";Subcadena([Relleno_Gráfico_15];1;[Tamaño_Barras_15]))
+
+/*PARA TABLAS SAMI_16*/
+=[SAMI_EGFF_16].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_16].[Nombre del Municipio])
+=[SAMI_EGFF_16].[Fuente]
+=[SAMI_EGFF_16].[Grupo Gasto]
+=[SAMI_EGFF_16].[Nombre]
+=Mayúsculas([SAMI_EGFF_16].[Fuente] + " - " + [SAMI_EGFF_16].[Descripcion Fuente])
+=[SAMI_EGFF_16].[Asignado]
+=[Modificación_16]
+=[SAMI_EGFF_16].[Vigente]
+=[SAMI_EGFF_16].[Devengado]
+=[SAMI_EGFF_16].[Pagado]
+=Si(EsError([%_de_Ejecución_16]);"División 0/0";[%_de_Ejecución_16]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_16];1;[Tamaño_Barras_16]));"División 0/0";Subcadena([Relleno_Gráfico_16];1;[Tamaño_Barras_16]))
+
+/*PARA TABLAS SAMI_17*/
+=[SAMI_EGFF_17].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_17].[Nombre del Municipio])
+=[SAMI_EGFF_17].[Fuente]
+=[SAMI_EGFF_17].[Grupo Gasto]
+=[SAMI_EGFF_17].[Nombre]
+=Mayúsculas([SAMI_EGFF_17].[Fuente] + " - " + [SAMI_EGFF_17].[Descripcion Fuente])
+=[SAMI_EGFF_17].[Asignado]
+=[Modificación_17]
+=[SAMI_EGFF_17].[Vigente]
+=[SAMI_EGFF_17].[Devengado]
+=[SAMI_EGFF_17].[Pagado]
+=Si(EsError([%_de_Ejecución_17]);"División 0/0";[%_de_Ejecución_17]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_17];1;[Tamaño_Barras_17]));"División 0/0";Subcadena([Relleno_Gráfico_17];1;[Tamaño_Barras_17]))
+
+/*PARA TABLAS SAMI_18*/
+=[SAMI_EGFF_18].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_18].[Nombre del Municipio])
+=[SAMI_EGFF_18].[Fuente]
+=[SAMI_EGFF_18].[Grupo Gasto]
+=[SAMI_EGFF_18].[Nombre]
+=Mayúsculas([SAMI_EGFF_18].[Fuente] + " - " + [SAMI_EGFF_18].[Descripcion Fuente])
+=[SAMI_EGFF_18].[Asignado]
+=[Modificación_18]
+=[SAMI_EGFF_18].[Vigente]
+=[SAMI_EGFF_18].[Devengado]
+=[SAMI_EGFF_18].[Pagado]
+=Si(EsError([%_de_Ejecución_18]);"División 0/0";[%_de_Ejecución_18]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_18];1;[Tamaño_Barras_18]));"División 0/0";Subcadena([Relleno_Gráfico_18];1;[Tamaño_Barras_18]))
+
+/*PARA TABLAS SAMI_19*/
+=[SAMI_EGFF_19].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_19].[Nombre del Municipio])
+=[SAMI_EGFF_19].[Fuente]
+=[SAMI_EGFF_19].[Grupo Gasto]
+=[SAMI_EGFF_19].[Nombre]
+=Mayúsculas([SAMI_EGFF_19].[Fuente] + " - " + [SAMI_EGFF_19].[Descripcion Fuente])
+=[SAMI_EGFF_19].[Asignado]
+=[Modificación_19]
+=[SAMI_EGFF_19].[Vigente]
+=[SAMI_EGFF_19].[Devengado]
+=[SAMI_EGFF_19].[Pagado]
+=Si(EsError([%_de_Ejecución_19]);"División 0/0";[%_de_Ejecución_19]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_19];1;[Tamaño_Barras_19]));"División 0/0";Subcadena([Relleno_Gráfico_19];1;[Tamaño_Barras_19]))
+
+/*PARA TABLAS SAMI_20*/
+=[SAMI_EGFF_20].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_20].[Nombre del Municipio])
+=[SAMI_EGFF_20].[Fuente]
+=[SAMI_EGFF_20].[Grupo Gasto]
+=[SAMI_EGFF_20].[Nombre]
+=Mayúsculas([SAMI_EGFF_20].[Fuente] + " - " + [SAMI_EGFF_20].[Descripcion Fuente])
+=[SAMI_EGFF_20].[Asignado]
+=[Modificación_20]
+=[SAMI_EGFF_20].[Vigente]
+=[SAMI_EGFF_20].[Devengado]
+=[SAMI_EGFF_20].[Pagado]
+=Si(EsError([%_de_Ejecución_20]);"División 0/0";[%_de_Ejecución_20]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_20];1;[Tamaño_Barras_20]));"División 0/0";Subcadena([Relleno_Gráfico_20];1;[Tamaño_Barras_20]))
+
+/*PARA TABLAS SAMI_21*/
+=[SAMI_EGFF_21].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_21].[Nombre del Municipio])
+=[SAMI_EGFF_21].[Fuente]
+=[SAMI_EGFF_21].[Grupo Gasto]
+=[SAMI_EGFF_21].[Nombre]
+=Mayúsculas([SAMI_EGFF_21].[Fuente] + " - " + [SAMI_EGFF_21].[Descripcion Fuente])
+=[SAMI_EGFF_21].[Asignado]
+=[Modificación_21]
+=[SAMI_EGFF_21].[Vigente]
+=[SAMI_EGFF_21].[Devengado]
+=[SAMI_EGFF_21].[Pagado]
+=Si(EsError([%_de_Ejecución_21]);"División 0/0";[%_de_Ejecución_21]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_21];1;[Tamaño_Barras_21]));"División 0/0";Subcadena([Relleno_Gráfico_21];1;[Tamaño_Barras_21]))
+
+/*PARA TABLAS SAMI_22*/
+=[SAMI_EGFF_22].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_22].[Nombre del Municipio])
+=[SAMI_EGFF_22].[Fuente]
+=[SAMI_EGFF_22].[Grupo Gasto]
+=[SAMI_EGFF_22].[Nombre]
+=Mayúsculas([SAMI_EGFF_22].[Fuente] + " - " + [SAMI_EGFF_22].[Descripcion Fuente])
+=[SAMI_EGFF_22].[Asignado]
+=[Modificación_22]
+=[SAMI_EGFF_22].[Vigente]
+=[SAMI_EGFF_22].[Devengado]
+=[SAMI_EGFF_22].[Pagado]
+=Si(EsError([%_de_Ejecución_22]);"División 0/0";[%_de_Ejecución_22]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_22];1;[Tamaño_Barras_22]));"División 0/0";Subcadena([Relleno_Gráfico_22];1;[Tamaño_Barras_22]))
+
+/*PARA TABLAS SAMI_23*/
+=[SAMI_EGFF_23].[Ejercicio]
+=Mayúsculas([SAMI_EGFF_23].[Nombre del Municipio])
+=[SAMI_EGFF_23].[Fuente]
+=[SAMI_EGFF_23].[Grupo Gasto]
+=[SAMI_EGFF_23].[Nombre]
+=Mayúsculas([SAMI_EGFF_23].[Fuente] + " - " + [SAMI_EGFF_23].[Descripcion Fuente])
+=[SAMI_EGFF_23].[Asignado]
+=[Modificación_23]
+=[SAMI_EGFF_23].[Vigente]
+=[SAMI_EGFF_23].[Devengado]
+=[SAMI_EGFF_23].[Pagado]
+=Si(EsError([%_de_Ejecución_23]);"División 0/0";[%_de_Ejecución_23]+"%")
+=Si(EsError(Subcadena([Relleno_Gráfico_23];1;[Tamaño_Barras_23]));"División 0/0";Subcadena([Relleno_Gráfico_23];1;[Tamaño_Barras_23]))
