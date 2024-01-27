1e3619e0f4ced76d923878934b2b7f2bb194401d
Kenet Orellana <86451633+KenetOrellana@users.noreply.github.com>
2024-01-27 15:38:59 -0600

    Portal_de_Transparencia_SAMI_EGFF_Orellana_Kenet

---
+++ Dashboards/Portal_de_Transparencia_SAMI_EGFF_Orellana_Kenet/reports.js
@@ -0,0 +1,28 @@
+/* Elaborado por Kenet Orellana para el Centro de Servicios a Usuarios (CSU) de la Secretaría de Finanzas (SEFIN) */
+var reportIds = {
+  "SAMI_13": {"name" : "SAMI_13"},
+  "SAMI_14": {"name" : "SAMI_14"},
+  "SAMI_15": {"name" : "SAMI_15"},
+  "SAMI_16": {"name" : "SAMI_16"},
+  "SAMI_17": {"name" : "SAMI_17"},
+  "SAMI_18": {"name" : "SAMI_18"},
+  "SAMI_19": {"name" : "SAMI_19"},
+  "SAMI_20": {"name" : "SAMI_20"},
+  "SAMI_21": {"name" : "SAMI_21"},
+  "SAMI_22": {"name" : "SAMI_22"},
+  "SAMI_23": {"name" : "SAMI_23"}
+};
+
+var order = {
+ "0": "SAMI_13",
+ "1": "SAMI_14",
+ "2": "SAMI_15",
+ "3": "SAMI_16",
+ "4": "SAMI_17",
+ "5": "SAMI_18",
+ "6": "SAMI_19",
+ "7": "SAMI_20",
+ "8": "SAMI_21",
+ "9": "SAMI_22",
+ "10": "SAMI_23"
+};
